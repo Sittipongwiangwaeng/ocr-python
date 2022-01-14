9e00a8de1b3557f22a3ef7388baec01a1fa418d7
Sittipong Wiangwaeng <sittipong.w@xeersoft.com>
2022-01-14 13:48:18 +0700

    add route readcard 6

--- app.py
+++ app.py
@@ -9,255 +9,251 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/readcard", methods=["POST"])
-def readcard():
+# @app.route("/readcard", methods=["POST"])
+# def readcard():
 
-    corpus = []
-    str = ""
-    strData = str
-    idCard = str
-    nameTh = str
-    lastnameTh = str
-    nameEng = str
-    lastnameEng = str
-    dateOfBirth = str
-    dateOfBirthTh = str
-    religion = str
-    address = str
-    custom_config = r'-l tha+eng --oem 3 --psm 6 -c language_model_ngram_space_delimited_language=1'
-    img = cv2.imread('download.jpeg')
+#     corpus = []
+#     str = ""
+#     strData = str
+#     idCard = str
+#     nameTh = str
+#     lastnameTh = str
+#     nameEng = str
+#     lastnameEng = str
+#     dateOfBirth = str
+#     dateOfBirthTh = str
+#     religion = str
+#     address = str
+#     custom_config = r'-l tha+eng --oem 3 --psm 6 -c language_model_ngram_space_delimited_language=1'
+#     img = cv2.imread('download.jpeg')
 
-    def get_grayscale(image):
-        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-    def thresholding(image):
-        return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#     def get_grayscale(image):
+#         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-    def clean(text):
-        cleanText = ""
-        cleanText = text.translate(
-            {ord(c): None for c in "_—?!=+-()[]*&%#|«»:;'‘“,"})
-        return cleanText
+#     def thresholding(image):
+#         return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 
-    def is_ascii(s):
-        return all(ord(c) < 128 for c in s)
-    gray = get_grayscale(img)
-    thresh = thresholding(gray)
-    img = thresh
-    text = pytesseract.image_to_string(img, config=custom_config)
-    strData = text
-    data = pytesseract.image_to_data(
-        img, config=custom_config, output_type=Output.DICT)
-    keys = list(data.keys())
+#     def clean(text):
+#         cleanText = ""
+#         cleanText = text.translate({ord(c): None for c in "_—?!=+-()[]*&%#|«»:;'‘“,"})
+#         return cleanText
 
-    totalBox = len(data['text'])
-    for i in range(totalBox):
-        (x, y, w, h) = (data['left'][i], data['top']
-                        [i], data['width'][i], data['height'][i])
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    str = ''.join(data['text'])
 
-    list(corpus)
-    strData = clean(strData)
-    findBD = strData.find("เกิดวันที่")
-    findReligion = strData.find("ศาสนา")
-    findAddress = strData.find("ที่อยู่")
-    findID = strData.find("Identification Number")
-    findIDv2 = strData.find("เลขประจําตัวประชาชน")
-    findIDv3 = strData.find("เลขประจ่าตัวประชาชน")
-    if findID != -1:
-        thisFindID = findID
-        checkID = 0
-        for x in range(thisFindID+22, thisFindID+39):
-            if checkID >= 2:
-                break
-            if strData[x].isspace() == True:
-                checkID = checkID+1
-            else:
-                checkID = 0
-                idCard = idCard + strData[x]
-    elif findIDv2 != -1:
-        thisFindID = findIDv2
-        checkID = 0
-        for x in range(thisFindID+20, thisFindID+39):
-            if checkID >= 2:
-                break
-            if strData[x].isspace() == True:
-                checkID = checkID+1
-            else:
-                checkID = 0
-                idCard = idCard + strData[x]
-    elif findIDv3 != -1:
-        thisFindID = findIDv3
-        checkID = 0
-        for x in range(thisFindID+20, thisFindID+39):
-            if checkID >= 2:
-                break
-            if strData[x].isspace() == True:
-                checkID = checkID+1
-            else:
-                checkID = 0
-                idCard = idCard + strData[x]
+#     def is_ascii(s):
+#         return all(ord(c) < 128 for c in s)
+#     gray = get_grayscale(img)
+#     thresh = thresholding(gray)
+#     img = thresh
+#     text = pytesseract.image_to_string(img, config=custom_config)
+#     strData = text
+#     data = pytesseract.image_to_data(img, config=custom_config, output_type=Output.DICT)
+#     keys = list(data.keys())
 
-    findNameTh = strData.find("ชื่อตัวและชื่อสกุล")
-    findNameThv2 = strData.find("ชื่อตัวและชื่อสกล")
-    if findNameTh != -1:
-        thisFindNameTh = findNameTh
-        checkNameTh = 0
-        keepName = 0
-        for x in range(thisFindNameTh+19, thisFindNameTh+60):
-            if checkNameTh >= 2:
-                break
-            if strData[x].isspace() == True:
-                checkNameTh = checkNameTh+1
-                keepName += 1
-            else:
-                checkNameTh = 0
-            if keepName == 1:
-                nameTh = nameTh + strData[x]
-            elif keepName == 2:
-                lastnameTh = lastnameTh + strData[x]
+#     totalBox = len(data['text'])
+#     for i in range(totalBox):
+#         (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+#         img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#     str = ''.join(data['text'])
 
-    elif findNameThv2 != -1:
-        thisFindNameTh = findNameThv2
-        checkNameTh = 0
-        keepName = 0
-        for x in range(thisFindNameTh+19, thisFindNameTh+60):
-            if checkNameTh >= 2:
-                break
-            if strData[x].isspace() == True:
-                checkNameTh = checkNameTh+1
-                keepName += 1
-            else:
-                checkNameTh = 0
+#     list(corpus)
+#     strData = clean(strData)
+#     findBD = strData.find("เกิดวันที่")
+#     findReligion = strData.find("ศาสนา")
+#     findAddress = strData.find("ที่อยู่")
+#     findID = strData.find("Identification Number")
+#     findIDv2 = strData.find("เลขประจําตัวประชาชน")
+#     findIDv3 = strData.find("เลขประจ่าตัวประชาชน")
+#     if findID != -1:
+#         thisFindID = findID
+#         checkID = 0
+#         for x in range(thisFindID+22, thisFindID+39):
+#             if checkID >= 2:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkID = checkID+1
+#             else:
+#                 checkID = 0
+#                 idCard = idCard + strData[x]
+#     elif findIDv2 != -1:
+#         thisFindID = findIDv2
+#         checkID = 0
+#         for x in range(thisFindID+20, thisFindID+39):
+#             if checkID >= 2:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkID = checkID+1
+#             else:
+#                 checkID = 0
+#                 idCard = idCard + strData[x]
+#     elif findIDv3 != -1:
+#         thisFindID = findIDv3
+#         checkID = 0
+#         for x in range(thisFindID+20, thisFindID+39):
+#             if checkID >= 2:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkID = checkID+1
+#             else:
+#                 checkID = 0
+#                 idCard = idCard + strData[x]
 
-            if keepName == 1:
-                nameTh = nameTh + strData[x]
-            elif keepName == 2:
-                lastnameTh = lastnameTh + strData[x]
+#     findNameTh = strData.find("ชื่อตัวและชื่อสกุล")
+#     findNameThv2 = strData.find("ชื่อตัวและชื่อสกล")
+#     if findNameTh != -1:
+#         thisFindNameTh = findNameTh
+#         checkNameTh = 0
+#         keepName = 0
+#         for x in range(thisFindNameTh+19, thisFindNameTh+60):
+#             if checkNameTh >= 2:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkNameTh = checkNameTh+1
+#                 keepName += 1
+#             else:
+#                 checkNameTh = 0
+#             if keepName == 1:
+#                 nameTh = nameTh + strData[x]
+#             elif keepName == 2:
+#                 lastnameTh = lastnameTh + strData[x]
 
-    findNameEng = strData.find("Name")
-    findNameEngv2 = strData.find("Nem")
-    if findNameEng != -1:
-        thisFindNameEng = findNameEng
-        checkNameEng = 0
-        for x in range(thisFindNameEng+4, thisFindNameEng+25):
-            if checkNameEng >= 3:
-                break
-            if strData[x].isspace() == True:
-                checkNameEng = checkNameEng+1
-            else:
-                checkNameEng = 0
-                nameEng = nameEng + strData[x]
-    elif findNameEngv2 != -1:
-        thisFindNameEng = findNameEngv2
-        checkNameEng = 0
-        for x in range(thisFindNameEng+3, thisFindNameEng+25):
-            if checkNameEng >= 3:
-                break
-            if strData[x].isspace() == True:
-                checkNameEng = checkNameEng+1
-            else:
-                checkNameEng = 0
-                nameEng = nameEng + strData[x]
+#     elif findNameThv2 != -1:
+#         thisFindNameTh = findNameThv2
+#         checkNameTh = 0
+#         keepName = 0
+#         for x in range(thisFindNameTh+19, thisFindNameTh+60):
+#             if checkNameTh >= 2:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkNameTh = checkNameTh+1
+#                 keepName += 1
+#             else:
+#                 checkNameTh = 0
+#             if keepName == 1:
+#                 nameTh = nameTh + strData[x]
+#             elif keepName == 2:
+#                 lastnameTh = lastnameTh + strData[x]
 
-    findLastname = strData.find("Lastname")
-    findLastnamev2 = strData.find("Last name")
-    findLastnamev3 = strData.find("lastname")
-    if findLastname != -1:
-        thisFindLastname = findLastname
-        checkLastnameEng = 0
-        for x in range(thisFindLastname+8, thisFindLastname+25):
-            if checkLastnameEng >= 2:
-                break
-            if strData[x].isalpha() == False:
-                checkLastnameEng = checkLastnameEng+1
-            else:
-                checkLastnameEng = 0
-                lastnameEng = lastnameEng + strData[x]
-    elif findLastnamev2 != -1:
-        thisFindLastname = findLastnamev2
-        checkLastnameEng = 0
-        for x in range(thisFindLastname+9, thisFindLastname+26):
-            if checkLastnameEng >= 2:
-                break
-            if strData[x].isalpha() == False:
-                checkLastnameEng = checkLastnameEng+1
-            else:
-                checkLastnameEng = 0
-                lastnameEng = lastnameEng + strData[x]
-    elif findLastnamev3 != -1:
-        thisFindLastname = findLastnamev3
-        checkLastnameEng = 0
-        for x in range(thisFindLastname+8, thisFindLastname+25):
-            if checkLastnameEng >= 2:
-                break
-            if strData[x].isalpha() == False:
-                checkLastnameEng = checkLastnameEng+1
-            else:
-                checkLastnameEng = 0
-                lastnameEng = lastnameEng + strData[x]
+#     findNameEng = strData.find("Name")
+#     findNameEngv2 = strData.find("Nem")
+#     if findNameEng != -1:
+#         thisFindNameEng = findNameEng
+#         checkNameEng = 0
+#         for x in range(thisFindNameEng+4, thisFindNameEng+25):
+#             if checkNameEng >= 3:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkNameEng = checkNameEng+1
+#             else:
+#                 checkNameEng = 0
+#                 nameEng = nameEng + strData[x]
+#     elif findNameEngv2 != -1:
+#         thisFindNameEng = findNameEngv2
+#         checkNameEng = 0
+#         for x in range(thisFindNameEng+3, thisFindNameEng+25):
+#             if checkNameEng >= 3:
+#                 break
+#             if strData[x].isspace() == True:
+#                 checkNameEng = checkNameEng+1
+#             else:
+#                 checkNameEng = 0
+#                 nameEng = nameEng + strData[x]
 
-    findDoB = strData.find("Date of Birth")
-    findDoBv2 = strData.find("Dets of Birth")
-    findDoBv3 = strData.find("Date of Buth")
-    if findDoB != -1:
-        thisFindDoB = findDoB
-        checkDoB = 0
-        for x in range(thisFindDoB+14, thisFindDoB+27):
-            # if checkDoB >= 2:
-            #     break
-            # if strData[x].isspace() == True:
-            #     checkDoB = checkDoB+1
-            # else:
-            #     checkDoB = 0
-            dateOfBirth = dateOfBirth + strData[x]
+#     findLastname = strData.find("Lastname")
+#     findLastnamev2 = strData.find("Last name")
+#     findLastnamev3 = strData.find("lastname")
+#     if findLastname != -1:
+#         thisFindLastname = findLastname
+#         checkLastnameEng = 0
+#         for x in range(thisFindLastname+8, thisFindLastname+25):
+#             if checkLastnameEng >= 2:
+#                 break
+#             if strData[x].isalpha() == False:
+#                 checkLastnameEng = checkLastnameEng+1
+#             else:
+#                 checkLastnameEng = 0
+#                 lastnameEng = lastnameEng + strData[x]
+#     elif findLastnamev2 != -1:
+#         thisFindLastname = findLastnamev2
+#         checkLastnameEng = 0
+#         for x in range(thisFindLastname+9, thisFindLastname+26):
+#             if checkLastnameEng >= 2:
+#                 break
+#             if strData[x].isalpha() == False:
+#                 checkLastnameEng = checkLastnameEng+1
+#             else:
+#                 checkLastnameEng = 0
+#                 lastnameEng = lastnameEng + strData[x]
+#     elif findLastnamev3 != -1:
+#         thisFindLastname = findLastnamev3
+#         checkLastnameEng = 0
+#         for x in range(thisFindLastname+8, thisFindLastname+25):
+#             if checkLastnameEng >= 2:
+#                 break
+#             if strData[x].isalpha() == False:
+#                 checkLastnameEng = checkLastnameEng+1
+#             else:
+#                 checkLastnameEng = 0
+#                 lastnameEng = lastnameEng + strData[x]
 
-    elif findDoBv2 != -1:
-        thisFindDoB = findDoBv2
-        checkDoB = 0
-        for x in range(thisFindDoB+14, thisFindDoB+27):
-            dateOfBirth = dateOfBirth + strData[x]
-    elif findDoBv3 != -1:
-        thisFindDoB = findDoBv3
-        checkDoB = 0
-        for x in range(thisFindDoB+13, thisFindDoB+27):
-            dateOfBirth = dateOfBirth + strData[x]
+#     findDoB = strData.find("Date of Birth")
+#     findDoBv2 = strData.find("Dets of Birth")
+#     findDoBv3 = strData.find("Date of Buth")
+#     if findDoB != -1:
+#         thisFindDoB = findDoB
+#         checkDoB = 0
+#         for x in range(thisFindDoB+14, thisFindDoB+27):
+#             # if checkDoB >= 2:
+#             #     break
+#             # if strData[x].isspace() == True:
+#             #     checkDoB = checkDoB+1
+#             # else:
+#             #     checkDoB = 0
+#             dateOfBirth = dateOfBirth + strData[x]
 
-    checkDoBth = 0
-    for x in range(findBD+10, findBD+30):
-        if checkDoBth >= 2:
-            break
-        if strData[x].isspace() == True:
-            checkDoBth = checkDoBth+1
-        else:
-            checkDoBth = 0
-            dateOfBirthTh = dateOfBirthTh + strData[x]
+#     elif findDoBv2 != -1:
+#         thisFindDoB = findDoBv2
+#         checkDoB = 0
+#         for x in range(thisFindDoB+14, thisFindDoB+27):
+#             dateOfBirth = dateOfBirth + strData[x]
+#     elif findDoBv3 != -1:
+#         thisFindDoB = findDoBv3
+#         checkDoB = 0
+#         for x in range(thisFindDoB+13, thisFindDoB+27):
+#             dateOfBirth = dateOfBirth + strData[x]
 
-    checkReligion = 0
-    for x in range(findReligion+5, findReligion+30):
-        if strData[x].isspace() == True:
-            checkReligion = checkReligion+1
-        else:
-            checkReligion = 0
-            religion = religion + strData[x]
+#     checkDoBth = 0
+#     for x in range(findBD+10, findBD+30):
+#         if checkDoBth >= 2:
+#             break
+#         if strData[x].isspace() == True:
+#             checkDoBth = checkDoBth+1
+#         else:
+#             checkDoBth = 0
+#             dateOfBirthTh = dateOfBirthTh + strData[x]
 
-    print("ID Card:", idCard)
-    print("ชื่อตัว:", nameTh)
-    print("ชื่อสกุล:", lastnameTh)
-    print("เกิดวันที่:", dateOfBirthTh)
-    print("Name:", nameEng)
-    print("Last name:", lastnameEng)
-    print("Date Of Birth:", dateOfBirth)
+#     checkReligion = 0
+#     for x in range(findReligion+5, findReligion+30):
+#         if strData[x].isspace() == True:
+#             checkReligion = checkReligion+1
+#         else:
+#             checkReligion = 0
+#             religion = religion + strData[x]
 
-    return jsonify(
-        idcard="idCard",
-        nameth="nameTh",
-        lastnameth="lastnameTh",
-        nameeng="nameEng",
-        lastnameeng="lastnameEng",
-        dateofbirth="dateOfBirth"
-    )
+#     print("ID Card:", idCard)
+#     print("ชื่อตัว:", nameTh)
+#     print("ชื่อสกุล:", lastnameTh)
+#     print("เกิดวันที่:", dateOfBirthTh)
+#     print("Name:", nameEng)
+#     print("Last name:", lastnameEng)
+#     print("Date Of Birth:", dateOfBirth)
+
+#     return jsonify(
+#         idcard="idCard",
+#         nameth="nameTh",
+#         lastnameth="lastnameTh",
+#         nameeng="nameEng",
+#         lastnameeng="lastnameEng",
+#         dateofbirth="dateOfBirth"
+#     )
